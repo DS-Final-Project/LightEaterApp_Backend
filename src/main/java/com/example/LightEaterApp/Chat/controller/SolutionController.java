c8701414e519ae6a1e0718b73eade0b153b1bc8c
= <sjhyun0926@gmail.com>
2023-09-17 16:05:13 +0900

    [fix]9/15 Solution 리스트 데이터 로그 설정

--- src/main/java/com/example/LightEaterApp/Chat/controller/SolutionController.java
+++ src/main/java/com/example/LightEaterApp/Chat/controller/SolutionController.java
@@ -43,6 +43,8 @@ public class SolutionController {
             // 이메일로 사용자 엔티티 가져오기
             UserEntity userEntity = userService.retrieveByUserEmailByEntity(email);
 
+
+            //relation true 중복 데이터 쌓이는 거 수정
             // 사용자 관계를 확인하고 해당하는 솔루션 엔티티 추가
             if (userEntity.isRelation1()) {
                 SolutionEntity solutionEntity1 = new SolutionEntity();
