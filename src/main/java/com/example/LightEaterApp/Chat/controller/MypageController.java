9ae5b1898308f0f333ebb92309e640ca1f2d22ef
= <sjhyun0926@gmail.com>
2023-09-19 02:21:41 +0900

    [fix]9/18 mypage 의심문장 안나오는 것 수정

--- src/main/java/com/example/LightEaterApp/Chat/controller/MypageController.java
+++ src/main/java/com/example/LightEaterApp/Chat/controller/MypageController.java
@@ -40,6 +40,9 @@ public class MypageController {
             UserEntity userEntity = userService.retrieveByUserEmailByEntity(email);
 
             PreviousMypageResponsebodyDTO dto = new PreviousMypageResponsebodyDTO(chatEntity,userEntity);
+            dto.setDoubtText1("이게 노력하는사람 모습이가");
+            dto.setDoubtText1("본인이 존댓말한건 생각안하고 내기분이 나빠보인다니");
+
 
             MypageResponseDTO response = MypageResponseDTO.<MypageResponseBodyDTO>builder()
                     .data(dto)
